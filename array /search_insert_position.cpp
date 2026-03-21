25718e53364f40cd2f426a1cafc6b63984fbc3c1
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-03-21 17:28:58 +0530

    Create search_insert_position.cpp

---
+++ array /search_insert_position.cpp
@@ -0,0 +1,18 @@
+// Q.35 (leetcode)
+int searchInsert(vector<int>& arr, int target) {
+        int i=0;
+        int j=arr.size()-1;
+        while(i<=j){
+            int mid=(i+j)/2;
+            if(arr[mid]==target){
+                return mid;
+            }
+            else if(arr[mid]>target){
+                j=mid-1;
+            }
+            else{
+                i=mid+1;
+            }
+        }
+        return i;      
+    }
