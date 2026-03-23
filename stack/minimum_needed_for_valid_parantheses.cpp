9c9b01c603f413068524a885b80ab6712a46b9dc
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-03-24 00:08:42 +0530

    Update minimum_needed_for_valid_parantheses.cpp

--- stack/minimum_needed_for_valid_parantheses.cpp
+++ stack/minimum_needed_for_valid_parantheses.cpp
@@ -1,3 +1,4 @@
+//T.C O(n) and S.C O(N)
 string s;
 stack<char>st;
 int count=0;
@@ -15,3 +16,20 @@ else{
 }
 }
 return st.size()+count;
+
+
+//T.C O(n) and S.C O(1)
+string s;
+int left=0;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
+                left++;
+            }
+            else{
+                if(left==0) count++;
+                else left--;
+            }
+        }
+        return left+count;
+        
