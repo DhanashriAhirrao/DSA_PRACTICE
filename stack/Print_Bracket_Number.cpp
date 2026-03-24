ccb1ade2e5a606220645d4ec7adad4432cb1be09
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-03-24 22:03:21 +0530

    Create Print_Bracket_Number.cpp

---
+++ stack/Print_Bracket_Number.cpp
@@ -0,0 +1,18 @@
+vector<int> bracketNumbers(string str) {
+        // Your code goes here
+        vector<int>ans;
+        stack<int>st;
+        int count=0;
+        for(int i=0;i<str.size();i++){
+            if(str[i]=='('){
+                count++;
+                st.push(count);
+                ans.push_back(count);
+            }
+            else if(str[i]==')'){
+                ans.push_back(st.top());
+                st.pop();
+            }
+        }
+        return ans;
+    }
