afbd7f526c3601b5596c271bf019029b22082351
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-03-28 11:11:19 +0530

    Create next_grater_ele.cpp

---
+++ stack/next_grater_ele.cpp
@@ -0,0 +1,16 @@
+vector<int> nextGreaterElements(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v(n,-1);
+        stack<int>st;
+        for(int i=0;i<2*n;i++){
+            int idx=i%n;
+            while(!st.empty() && nums[st.top()]<nums[idx]){
+                v[st.top()]=nums[idx];
+                st.pop();
+            }
+            if(i<n){
+                st.push(idx);
+            }
+        }
+        return v;
+    }
