3b74edd7cbc58ac0d0ad66a877637967bc627c16
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-05-29 16:01:36 +0530

    Create basic.cpp

---
+++ linked_list/basic.cpp
@@ -0,0 +1,16 @@
+# entering element in null
+
+node *newnode =new node(30) #created 1st node  
+head=new node
+tail=new node
+node *newnode=new node(30) #new element connected
+tail=new node
+
+#enter new element and make it head
+node *newnode=new node(30) #1st element
+head=new node
+tail=new node
+node *newnode=new node(40) #second entered element and make it start/head
+new node->next=head
+head=new node  
+  
