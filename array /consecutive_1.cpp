386c9bd6eb665479f2445c7be107f5269c1d8a7a
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-06-03 00:13:47 +0530

    Create consecutive_1.cpp

---
+++ array /consecutive_1.cpp
@@ -0,0 +1,17 @@
+//leetcode Q.485
+// same question in bit manipulation 
+int findMaxConsecutiveOnes(vector<int>& nums) {
+        int cnt=0;
+        int mx=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(nums[i]==1){
+                cnt++;
+                mx=max(mx,cnt);
+            }
+            else{
+                cnt=0;
+            }
+        }
+        return mx;
+    }
