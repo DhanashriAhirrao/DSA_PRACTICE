de79dffa18f66086dae461614070df5d9d4faa9a
DhanashriAhirrao <dhanuahirrao07@gmail.com>
2026-06-11 10:33:19 +0530

    Create power_of_three.cpp

---
+++ recursion/power_of_three.cpp
@@ -0,0 +1,14 @@
+//Q.326 -> power of three
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        else if(n<=0 || n%3!=0){
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+};
